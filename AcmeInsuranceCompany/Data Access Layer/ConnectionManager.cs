ace7174c8b8984a2d10dc6b14641b65e7b9f96bb
Michael Ormond <michael.ormond@hotmail.com>
2019-02-26 20:19:48 +1000

    Created ConnectionManager class: connects to database to allow modification

---
+++ AcmeInsuranceCompany/Data Access Layer/ConnectionManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//added
+using System.Data.SqlClient;
+
+namespace AcmeInsuranceCompany.Data_Access_Layer
+{
+    class ConnectionManager
+    {
+        //establish connection between program and database
+        public static SqlConnection DatabaseConnection()
+        {
+            string connection = "Data Source=laptop-hq36brdd\\sqlexpress; Initial Catalog=Acme; User=sa; Password=sqlexpress";
+            SqlConnection conn = new SqlConnection(connection);
+            return conn;
+        }
+    }
+}
